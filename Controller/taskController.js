f7c379675657e763083076bd4515d6a6a969f6f7
spr887011 <spr887011@gmail.com>
2023-11-22 12:12:11 +0530

    edit summary and description done

--- Controller/taskController.js
+++ Controller/taskController.js
@@ -41,18 +41,31 @@ async function updateStatus(taskId,status) {
     return "Server Busy";
   }
   }
+  
+  async function updateSumarry(taskId,sumarry, description) {
+    try {
+      const taskToUpdate = await Task.findById(taskId);
+      if (!taskToUpdate) {
+        return "Task not found";
+      }
+      taskToUpdate.sumarry = sumarry;
+      taskToUpdate.description = description;
+      await taskToUpdate.save();
+      return true;
+    } catch (error) {
+      console.log(error);
+      return "Server Busy";
+    }
+    }
 
 async function DeleteQuestion(id, email) {
   try {
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return "Invalid";
     }
-    const question = await Questions.findOne({ _id: id });
-    if (question.userEmail === email) {
-      console.log(question.userEmail);
-      await Questions.deleteOne({ _id: id });
+    const task = await Task.findOne({ _id: id });
+      await task.deleteOne({ _id: id });
       return true;
-    }
   } catch (error) {
     console.log(error);
     return "Server Busy";
@@ -60,4 +73,4 @@ async function DeleteQuestion(id, email) {
 }
 
 
-module.exports = { CreateTask, GetAllTask,updateStatus, DeleteQuestion,  };
+module.exports = { CreateTask, GetAllTask,updateStatus, DeleteQuestion, updateSumarry,};
--- routes/issues.js
+++ routes/issues.js
@@ -5,6 +5,7 @@ const {
   CreateTask,
   GetAllTask,
   updateStatus,
+  updateSumarry,
   DeleteQuestion,
 } = require("../Controller/taskController");
 
@@ -43,6 +44,22 @@ router.patch("/update/status", async function (req, res, next) {
       res.status(400).send("Invalid");
     } else {
       res.json(await updateStatus(taskId,status));
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Server Busy");
+  }
+});
+
+router.post("/update/sumarry", async function (req, res, next) {
+  const auth_token = req.headers.authorization.split(" ")[1];
+  const {taskId,sumarry, description} = req.body;
+  try {
+    var loginCredentials = await AuthorizeUser(auth_token);
+    if (loginCredentials === false) {
+      res.status(400).send("Invalid");
+    } else {
+      res.json(await updateSumarry(taskId,sumarry, description));
     }
   } catch (error) {
     console.log(error);
